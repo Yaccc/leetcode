8f413779b84899e3555c12fe6ebc2715ffcca712
xiezhaodong <837199685@qq.com>
2015-05-09 13:10:38 +0800

    add one

---
+++ R/Reverse_Words_in_a_String.java
@@ -0,0 +1,25 @@
+ public String reverseWords(String s) {
+        String trim=" "+s.trim();
+		char[] chararray=trim.toCharArray();
+		String word="";
+		StringBuffer buffer=new StringBuffer();
+		for (int i = chararray.length-1; i>=0; i--) {
+
+			if(!String.valueOf(chararray[i]).equals(" ")){
+				word+=chararray[i];
+			}else{
+
+				if(buffer.toString().lastIndexOf(" ")!=buffer.length()-1){
+					//如果bu是最后一个有空格
+					buffer.append(" ");
+				}
+
+				StringBuffer re=new StringBuffer(word);
+				re.reverse();
+				buffer.append(re);
+				word="";
+			}
+
+		}
+		return buffer.toString().trim();
+    }
